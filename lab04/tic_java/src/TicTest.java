77c0830f4cc2ffe023657256d0ec785bfcc52f25
Andre-Lepage <andrelepage15@gmail.com>
2023-06-27 15:12:50 -0400

    is full test fail

--- lab04/tic_java/src/TicTest.java
+++ lab04/tic_java/src/TicTest.java
@@ -30,4 +30,19 @@ public class TicTest {
 		assertEquals(3, board.getNumberOfRows());
 	}
 	
+	@Test
+	void isFullTest() {
+		Tic board = new Tic (3,3);
+		board.place("X", 0, 0);
+		board.place("O", 0, 1);
+		board.place("X", 0, 2);
+		board.place("O", 1, 0);
+		board.place("X", 1, 1);
+		board.place("O", 1, 2);
+		board.place("X", 2, 0);
+		board.place("O", 2, 1);
+		board.place("X", 2, 2);
+		
+		assertEquals(true, board.isFull());	
+	}
 }
